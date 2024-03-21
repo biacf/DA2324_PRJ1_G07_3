67b413ce95cf30bad8460bac016d49ddb01be400
Beatriz Ferreira <biatizferreira04@gmail.com>
2024-03-21 01:21:08 +0000

    Fixed menu error

--- code/main.cpp
+++ code/main.cpp
@@ -14,22 +14,22 @@ int main() {
         std::cout << "\na. Load data to be parsed" << std::endl;
         std::cout << "b. Check Maximum Flow options" << std::endl;
         std::cout << "c. Check under supplied cities" << std::endl;
-        std::cout << "d. Remove Pumping Stations" << std::endl;
-        std::cout << "e. Vital Pipelines" << std::endl;
-        std::cout << "f. Remove pipelines" << std::endl;
-        std::cout << "g. Exit" << std::endl;
+        std::cout << "d. Remove Water Reservoirs" << std::endl;
+        std::cout << "e. Remove Pumping Stations" << std::endl;
+        std::cout << "f. Vital Pipelines" << std::endl;
+        std::cout << "g. Remove pipelines" << std::endl;
+        std::cout << "h. Exit" << std::endl;
 
         std::cout << "\nOption: ";
         std::cin >> option;
-        while(option < 'a' || option > 'g'){
+        while(option < 'a' || option > 'h'){
             std::cout << "\nOption: ";
             std::cin >> option;
         }
 
         switch(option){
-            case 'g':{
+            case 'h':{
                 return 0;
-                break;
             }
             default:{
                 return 0;
